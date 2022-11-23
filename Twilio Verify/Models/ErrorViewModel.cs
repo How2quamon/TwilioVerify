150898d735b7aad2608537f2ccc756d9b349c4ed
JohanTT <thejohan39@gmail.com>
2022-11-23 23:39:11 +0700

    back-Twilio

---
+++ Twilio Verify/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Twilio_Verify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
